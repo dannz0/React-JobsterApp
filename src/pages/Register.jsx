c216003cd0ef59dd0a1c1a6e15baed202b4d6a5c
dannz0 <freshiznogoud@gmail.com>
2023-04-08 03:05:01 +0200

    working on register/login page, added controlled inputs..

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,91 @@
+import { useState, useEffect } from 'react';
+import { Logo, FormRow } from '../components';
+import Wrapper from '../assets/wrappers/RegisterPage';
+import { toast } from 'react-toastify';
+import { useSelector, useDispatch } from 'react-redux';
+import { loginUser, registerUser } from '../features/user/userSlice';
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+};
+
 const Register = () => {
-  return <div>Register</div>;
+  const [values, setValues] = useState(initialState);
+  const { user, isLoading } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  const handleChange = function (e) {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setValues({ ...values, [name]: value });
+  };
+
+  const onSubmit = function (e) {
+    e.preventDefault();
+    const { name, email, password, isMember } = values;
+
+    if (!email || !password || (!isMember && !name)) {
+      toast.error('Please fill out all fields');
+      return;
+    }
+
+    if (!isMember) {
+      dispatch(registerUser({ name, email, password }));
+      return;
+    }
+
+    dispatch(loginUser({ email, password }));
+  };
+
+  const toggleMember = function () {
+    setValues({ ...values, isMember: !values.isMember });
+  };
+
+  return (
+    <Wrapper className='full-page'>
+      <form onSubmit={onSubmit} className='form'>
+        <Logo />
+        <h3>{values.isMember ? 'Login' : 'Register'}</h3>
+        {/* name field */}
+        {!values.isMember && (
+          <FormRow
+            type='text'
+            name='name'
+            value={values.name}
+            handleChange={handleChange}
+          />
+        )}
+
+        {/* email field */}
+        <FormRow
+          type='email'
+          name='email'
+          value={values.email}
+          handleChange={handleChange}
+        />
+
+        {/* pw field */}
+        <FormRow
+          type='password'
+          name='password'
+          value={values.password}
+          handleChange={handleChange}
+        />
+        <button className='btn btn-block' type='submit'>
+          submit
+        </button>
+        <p>
+          {values.isMember ? 'Not a member yet?' : 'Already a member?'}
+          <button type='button' onClick={toggleMember} className='member-btn'>
+            {values.isMember ? 'Register' : 'Login'}
+          </button>
+        </p>
+      </form>
+    </Wrapper>
+  );
 };
 export default Register;
