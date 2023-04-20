c638edd9b5aa73f9304d83a3e84621e525e20308
dannz0 <freshiznogoud@gmail.com>
2023-04-20 23:28:27 +0200

    fixed statsbug showing 0 when no jobs are added

--- src/pages/Dashboard/Stats.jsx
+++ src/pages/Dashboard/Stats.jsx
@@ -16,7 +16,7 @@ const Stats = () => {
   return (
     <>
       <StatsContainer />
-      {monthlyApplications.length && <ChartsContainer />}
+      {monthlyApplications.length > 0 && <ChartsContainer />}
     </>
   );
 };
